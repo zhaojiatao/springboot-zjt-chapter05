860ea6e808771ecd6d1f0abe4c589f700906f7df
zhaojiatao <2322795718@qq.com>
2018-01-22 22:46:09 +0800

    增加外部tomcat部署配置

--- src/main/java/com/zjt/chapter05/Chapter05Application.java
+++ src/main/java/com/zjt/chapter05/Chapter05Application.java
@@ -2,9 +2,16 @@ package com.zjt.chapter05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Chapter05Application {
+public class Chapter05Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Chapter05Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter05Application.class, args);
